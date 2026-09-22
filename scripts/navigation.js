e3e18821cf968b173ce367a0d49ae3c34cd30081
feranana <faaagmez@byupathway.edu>
2026-09-22 00:26:53 -0300

    new portfolio banner added

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,11 @@
 const hamButton = document.querySelector("#ham-btn");
 const navBar = document.querySelector("#nav-bar");
+const closePortfolioButton = document.querySelector("#close-portfolio");
+
+closePortfolioButton.addEventListener("click", () => {
+  const portfolioDiv = document.querySelector(".portfolio");
+  portfolioDiv.style.display = "none";
+});
 
 hamButton.addEventListener("click", () => {
   hamButton.classList.toggle("show");
